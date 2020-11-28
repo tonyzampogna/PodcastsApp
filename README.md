1418bd1a84bbde70a1b9e2e8e651105c5476c843
Tony Zampogna <tony.zampogna@gmail.com>
2020-11-27 18:23:59 -0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Podcasts API
+REST API calls that return podcast information.
+
+## Getting started
+
---
+++ src/main/java/com/tonyzampogna/podcast/PodcastsApplication.java
@@ -0,0 +1,13 @@
+package com.tonyzampogna.podcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastsApplication.class, args);
+	}
+
+}
